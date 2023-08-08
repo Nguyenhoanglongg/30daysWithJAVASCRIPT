421fad5b98de0474f03db4a7ad5cd83a8f32ab10
Nguyenhoanglongg <82560389+Nguyenhoanglongg@users.noreply.github.com>
2023-08-08 18:07:22 +0700

    Commit
    Gallery Images

---
+++ Days5/app.js
@@ -0,0 +1,59 @@
+const images = document.querySelectorAll(".wrapper .image img");
+const gallery = document.querySelector(".gallery");
+const galleryImg = document.querySelector(".gallery-inner img");
+const close = document.querySelector(".gallery .close");
+
+const next = document.querySelector(".control.next");
+const prev = document.querySelector(".control.prev");
+
+let currentIndex = 0;
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    currentIndex = index;
+    showGallery();
+  });
+});
+
+function showGallery() {
+  currentIndex == images.length - 1
+    ? next.classList.add("hide")
+    : next.classList.remove("hide");
+
+  currentIndex == 0
+    ? prev.classList.add("hide")
+    : prev.classList.remove("hide");
+
+  gallery.classList.add("show");
+  galleryImg.src = images[currentIndex].src;
+}
+
+close.addEventListener("click", () => {
+  gallery.classList.remove("show");
+});
+next.addEventListener("click", () => {
+  currentIndex != images.length - 1 ? currentIndex++ : undefined;
+  showGallery();
+});
+prev.addEventListener("click", () => {
+  currentIndex != 0 ? currentIndex-- : undefined;
+  showGallery();
+});
+
+// esc click
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    gallery.classList.remove("show");
+  }
+});
+//left keyboard event
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode == 37) currentIndex != 0 ? currentIndex-- : undefined;
+  showGallery();
+});
+//right keyboard event
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39)
+    currentIndex != images.length - 1 ? currentIndex++ : undefined;
+  showGallery();
+});
